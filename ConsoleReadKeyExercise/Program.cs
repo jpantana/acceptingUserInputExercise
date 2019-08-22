9b9032c08897ca9ab0907f1d3cdd2d05399f6306
jpantana <josh.pantana@gmail.com>
2019-08-22 14:54:45 -0500

    first commit

---
+++ ConsoleReadKeyExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleReadKeyExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo enteredKey;
+
+            do
+            {
+                enteredKey = Console.ReadKey();
+                Console.WriteLine($"You pressed the {enteredKey.Key.ToString()} key");
+
+            } while (enteredKey.Key != ConsoleKey.Escape);
+
+            string sentence;
+
+            do
+            {
+                Console.WriteLine("Type in a sentence. Press enter when done.");
+                sentence = Console.ReadLine();
+                Console.WriteLine($"You entered the sentence: {sentence}");
+
+            } while (sentence != "quit");
+
+        }
+    }
+}
